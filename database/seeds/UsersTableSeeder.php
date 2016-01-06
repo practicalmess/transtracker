1236d0d8db9bda73318370a985ca5582ae4ea511
SJ Hackney <snackney@gmail.com>
2016-01-06 17:34:03 -0500

    Added birthday, gender, and pronouns to user seeders

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,16 +11,28 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+    	//Necessary user columns
         $user = \App\User::firstOrCreate(['email' => 'jill@harvard.edu']);
 	    $user->name = 'Jill';
 	    $user->email = 'jill@harvard.edu';
 	    $user->password = \Hash::make('helloworld');
+	    //User profile columns
+	    $user->birthday = Carbon::createFromDate(1990, 5, 21);
+	    $user->gender = 'Woman';
+	    $user->pronouns = "she/her";
+
 	    $user->save();
 
 	    $user = \App\User::firstOrCreate(['email' => 'jamal@harvard.edu']);
 	    $user->name = 'Jamal';
 	    $user->email = 'jamal@harvard.edu';
 	    $user->password = \Hash::make('helloworld');
+
+	    //User profile columns
+	    $user->birthday = Carbon::createFromDate(1983, 10, 4);
+	    $user->gender = 'Man';
+	    $user->pronouns = "he/him";
+
 	    $user->save();
     }
 }
