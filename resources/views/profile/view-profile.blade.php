e369e51eccb127d7a00b271154556b0267da9ab9
SJ Hackney <snackney@gmail.com>
2016-01-07 15:10:53 -0500

    Troubleshooting default (0000-00-00) birthday display, 2

--- resources/views/profile/view-profile.blade.php
+++ resources/views/profile/view-profile.blade.php
@@ -19,7 +19,7 @@
 				?>
 			</script>
 			<!--Display birthday in readable format-->
-			@if ($bd->year === int(0000))
+			@if ($bd->year === 0000)
 				{{$bd->toFormattedDateString()}}
 			@endif
 		</div>
